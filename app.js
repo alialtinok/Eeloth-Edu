9bfd4fd8a89dfe61bdede5895bf70f117d76f1d8
Ali Altınok <info.alialtinok@gmail.com>
2022-03-30 01:06:53 +0300

    Edu Project Created && MVC Designed

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const pageRoute = require('./routes/pageRoute');
+
+const app = express();
+
+//Template Engine
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static("public"));
+
+//Routes
+app.use('/', pageRoute); 
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
