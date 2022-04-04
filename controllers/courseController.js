526c5092a57f17ef0c7583d0876354577dd0b99c
Ali Altınok <info.alialtinok@gmail.com>
2022-04-04 15:56:35 +0300

    single pages && slug's

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -30,3 +30,17 @@ exports.getAllCourses = async (req, res) => {
   }
 };
 
+exports.getCourse = async (req, res) => {
+  try {
+    const course = await Course.findOne({slug: req.params.slug});
+    res.status(200).render('course', {
+      course,
+      page_name: 'courses',
+    });
+  } catch (error){
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.route('/').post(courseController.createCourse); // http://localhost:3000/courses
 router.route('/').get(courseController.getAllCourses); // http://localhost:3000/courses
+router.route('/:slug').get(courseController.getCourse); // http://localhost:3000/courses/_id
 
 module.exports = router;
