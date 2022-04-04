24a621305405d1eada26f725b49bc13d0a078966
Ali Altınok <info.alialtinok@gmail.com>
2022-04-04 18:42:26 +0300

    created categories %% filtered

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
+
 
 const app = express();
 
@@ -28,6 +30,7 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 //Routes
 app.use('/', pageRoute); 
 app.use('/courses', courseRoute); 
+app.use('/categories', categoryRoute); 
 
 
 const port = 3000;
--- models/Course.js
+++ models/Course.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 const slugify = require('slugify');
+const Schema = mongoose.Schema;
 
 const CourseSchema = new Schema({
   name: {
@@ -18,18 +18,22 @@ const CourseSchema = new Schema({
     default: Date.now,
   },
   slug: {
-    type: String,
-    unique: true,
+    type:String,
+    unique: true
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
   }
-
 });
 
-CourseSchema.pre('validate', function(next) {
+CourseSchema.pre('validate', function(next){
   this.slug = slugify(this.name, {
-    lower: true,
-    strict:true,
-  });
-next();
-});
+    lower:true,
+    strict:true
+  })
+  next();
+})
+
 const Course = mongoose.model('Course', CourseSchema);
-module.exports = Course;
+module.exports = Course; 
