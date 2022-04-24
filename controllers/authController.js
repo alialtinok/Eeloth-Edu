20a48da0eb58e84e4d55097b4ca87b3df4171962
Ali Altınok <info.alialtinok@gmail.com>
2022-04-24 21:44:37 +0300

    user roles

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,6 +7,7 @@ exports.createUser = async (req, res) => {
       status: 'success',
       user,
     });
+    res.status(201).redirect('/login');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
